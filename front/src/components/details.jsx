0a739f55b354a1b24ff77897010626bd1307c382
Fabio Trech <fabiotrech@gmail.com>
2019-09-29 14:14:46 -0300

    Agregos estilos a la vista del producto

--- front/src/components/details.jsx
+++ front/src/components/details.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Breadcrumb from "./common/breadcrumb";
 import { getById } from "../services/itemService";
+import "./details.scss";
 
 class Details extends Component {
-    state = { 
-        data: {}
+    state = {
+        categories: ["Uno", "Dos", "Tres"],
+        data: {
+            item: {
+                price: {}
+            }
+        }
     }
 
     componentDidMount() {
@@ -13,8 +20,31 @@ class Details extends Component {
         this.setState({ data: item });
     }
 
-    render() { 
-        return ( <h1>Details</h1> );
+    render() {
+        const { data: {item}, categories } = this.state;
+
+        return (
+            <React.Fragment>
+                <Breadcrumb values={categories} />
+
+                <div className="details">
+                    <div className="content">
+                        <img src={item.picture} alt="Imagen producto" />
+
+                        <h2 className="description-title">Descripci&oacute;n del producto</h2>
+                        <p className="item-description">{item.description}</p>
+                    </div>
+                    <div className="buy-box">
+                        <p className="sold-quantity">Nuevo - {item.sold_quantity} vendidos</p>
+                        <h1 className="item-title">{item.title}</h1>
+                        <p className="price">{item.price.currency} {item.price.amount}</p>
+                        <button type="button">
+                            Comprar
+                        </button>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
     }
 }
  
--- front/src/services/itemService.js
+++ front/src/services/itemService.js
@@ -5,21 +5,19 @@ export function getById(id) {
             "name": "Fabio",
             "lastname": "Trech"
         },
-        item: [
-            {
-                "id": "MLA804207795",
-                "title": "Nuevo! iPod Touch 32 Gb Representante Oficial De Apple",
-                "price": {
-                    "currency": "$",
-                    "amount": 21.499,
-                    "decimals": null
-                },
-                "picture": "http://mla-s2-p.mlstatic.com/924888-MLA32011260568_082019-I.jpg",
-                "condition": "new",
-                "free_shipping": true,
-                "sold_quantit": 15,
-                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-        ]
+        "item": {
+            "id": "MLA804207795",
+            "title": "Nuevo! iPod Touch 32 Gb Representante Oficial De Apple",
+            "price": {
+                "currency": "$",
+                "amount": 21.499,
+                "decimals": null
+            },
+            "picture": "http://mla-s2-p.mlstatic.com/716505-MLA31597842150_072019-O.jpg",
+            "condition": "new",
+            "free_shipping": true,
+            "sold_quantity": 15,
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
     };
 }
